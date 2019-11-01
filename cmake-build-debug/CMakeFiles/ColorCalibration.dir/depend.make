d74ef49411bd446cb0012c24df9a472ec674cbf2
adrianzgaljic <adrian.zgaljic@gmail.com>
2019-11-01 18:28:59 +0100

    add cmake-build-debug folder

---
+++ cmake-build-debug/CMakeFiles/ColorCalibration.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ColorCalibration.
+# This may be replaced when dependencies are built.
---
+++ cmake-build-debug/CMakeFiles/opencv.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opencv.dir/main.cpp.o"
+  "opencv.pdb"
+  "opencv"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/opencv.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
